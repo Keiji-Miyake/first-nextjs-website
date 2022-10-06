eac3227a296754d4c51f855f37f01947d310f816
Keiji Miyake <k.miyake01+dev@gmail.com>
2022-10-06 23:46:35 +0900

    spell修正。imgタグをNextjsタグに変更

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import * as style from "../styles/common.module.scss"
 
@@ -5,10 +6,10 @@ const Footer = () => {
     return (
         <footer className={style.footerWrapper}>
             <div className={style.insideContainer}>
-                <a href="https://www.google.com/"><img src="/images/github.svg" alt="logo"/></a>
-                <a href="https://www.google.com/"><img src="/images/linkedin.svg" alt="logo"/></a>
-                <a href="https://www.google.com/"><img src="/images/twitter.svg" alt="logo"/></a>
-                <a href="https://www.google.com/"><img src="/images/facebook.svg" alt="logo"/></a>
+                <a href="https://www.google.com/"><Image src="/images/github.svg" alt="logo" /></a>
+                <a href="https://www.google.com/"><Image src="/images/linkedin.svg" alt="logo" /></a>
+                <a href="https://www.google.com/"><Image src="/images/twitter.svg" alt="logo" /></a>
+                <a href="https://www.google.com/"><Image src="/images/facebook.svg" alt="logo" /></a>
                 <hr/>
                 <Link href="/blog"><a>Blog</a></Link>
                 <Link href="/contact"><a>Contact</a></Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Index = () => {
       <div className={style.hero}>
         <Image src="/images/index-hero.jpg" alt="hero" layout="fill" objectFit="cover" quality={90} />
         <div className={style.textContainer}>
-          <h1>I'm Abe Hiroki!</h1>
+          <h1>I&#39;m Abe Hiroki!</h1>
           <h3>JavaScript Developer</h3>
         </div>
       </div>
@@ -19,7 +19,7 @@ const Index = () => {
         <div className={style.profile}>
           <div>
             <h2>JavaScript Nerd</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
           <Image src="/images/profile.jpg" alt="hero" height={1195} width={1000} quality={90} />
         </div>
@@ -27,10 +27,10 @@ const Index = () => {
         <div className={style.skills}>
           <h2>Skills</h2>
           <div className={style.skillsContainer}>
-            <div><img src="/images/javascript.svg" alt="javascript" /><span>JavaScript / 10 years</span></div>
-            <div><img src="/images/react.svg" alt="react" /><span>React / 5 years</span></div>
-            <div><img src="/images/gatsby.svg" alt="gatsby" /><span>Gatsby / 3 years</span></div>
-            <div><img src="/images/next.svg" alt="next" /><span>Next.JS / 3 years</span></div>
+            <div><Image src="/images/javascript.svg" alt="javascript" /><span>JavaScript / 10 years</span></div>
+            <div><Image src="/images/react.svg" alt="react" /><span>React / 5 years</span></div>
+            <div><Image src="/images/gatsby.svg" alt="gatsby" /><span>Gatsby / 3 years</span></div>
+            <div><Image src="/images/next.svg" alt="next" /><span>Next.JS / 3 years</span></div>
           </div>
         </div>
 
